39bad5122526bf1d0860c2b102251ad7c1cf3966
bohan <bw1417@nyu.edu>
2019-06-04 11:29:01 -0400

    New Assignment

---
+++ tests/test_pointers.hpp
@@ -0,0 +1,13 @@
+//
+//  test_pointers.hpp
+//  OOP
+//
+//  Created by apple on 2019/6/4.
+//
+
+#ifndef test_pointers_hpp
+#define test_pointers_hpp
+
+#include <stdio.h>
+
+#endif /* test_pointers_hpp */
